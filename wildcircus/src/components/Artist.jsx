39d193d21499b5c29a41959899a2a683bdc8b489
XeniaR1S <lecomtep.x@gmail.com>
2020-01-30 15:58:31 +0100

    Finish artist component, sorry not atomic commit

--- wildcircus/src/components/Artist.jsx
+++ wildcircus/src/components/Artist.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ClicFluc from '../img/laugh.jpg'
 import Aladdin from '../img/dream.jpg'
 import Loyal from '../img/marvel.jpg'
+
 import '../App.css';
 
 class Artist extends React.Component {
@@ -10,6 +11,7 @@ class Artist extends React.Component {
     return (
       <div className="artist">
         <p className='artist-title'>ARTIST</p>
+
         <div className='artist-card'>
           <figure className='figure-laugh'>
             <img className='img-laugh' src={ClicFluc} alt='Photo of Clic and Fluc'/>
@@ -20,6 +22,7 @@ class Artist extends React.Component {
             <li>Speciality : Cream pie in face</li>
           </ul>
         </div>
+
         <div className='artist-card'>
           <figure className='figure-dream'>
             <img className='img-dream' src={Aladdin} alt='Photo of Aladdin'/>
@@ -30,6 +33,7 @@ class Artist extends React.Component {
             <li>Speciality : Acrobatics</li>
           </ul>
         </div>
+
         <div className='artist-card'>
           <figure className='figure-marvel'>
             <img className='img-marvel' src={Loyal} alt='Photo of Mr Loyal and his Lions'/>
